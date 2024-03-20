d3d114a2fb4fed9c5d2f5e3cd5f3429f380ed611
GilbertHan1011 <GilbertHan1011@gmail.com>
2024-03-20 17:38:35 +0800

    fix scarches error

--- TrajAtlas/TrajDiff/trajDiff_2.py
+++ TrajAtlas/TrajDiff/trajDiff_2.py
@@ -685,20 +685,21 @@ class Tdiff:
     def _test_binom(self,
                     length_df,
                     times:int = 20):
-    sumVal=length_df["true"]+length_df["false"]
-    trueVal=length_df["true"]
-    null=length_df["null"]
-    p_val_list=[]
-    for i in range(len(length_df)):
-        if null[i]==0:
-            null[i]=1/(sumVal[i]*times) # minimal
-        p_val= 1- binom.cdf(trueVal[i], sumVal[i], null[i])
-        if trueVal[i] == 0:
-            p_val=1
-        #p_val=1-poisson.cdf(rate[i], null[i])
-        p_val_list.append(p_val)
-    length_df["binom_p"]=p_val_list
-    return(length_df)
+        print("test")
+        sumVal = length_df["true"] + length_df["false"]
+        trueVal=length_df["true"]
+        null=length_df["null"]
+        p_val_list=[]
+        for i in range(len(length_df)):
+            if null[i]==0:
+                null[i]=1/(sumVal[i]*times) # minimal
+            p_val= 1- binom.cdf(trueVal[i], sumVal[i], null[i])
+            if trueVal[i] == 0:
+                p_val=1
+            #p_val=1-poisson.cdf(rate[i], null[i])
+            p_val_list.append(p_val)
+        length_df["binom_p"]=p_val_list
+        return(length_df)
 
     def permute_test_point(self,
                            mdata: MuData,
